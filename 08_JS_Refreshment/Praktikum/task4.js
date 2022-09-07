0b1e94e61f3198bcfa2d3df038bc59a5597e5727
riorasyid23 <rio.rasyid23@gmail.com>
2022-09-07 17:12:43 +0700

    Completed-task/08_JS_Refreshment: Penambahan File Summary dan Praktikum (Done)

---
+++ 08_JS_Refreshment/Praktikum/task4.js
@@ -0,0 +1,4 @@
+let bdays= ['10-17', '05-19', '20-19']
+
+const replace = bdays.map((newBdays) => newBdays.replace('-','/'))
+console.log(replace)
---
+++ 08_JS_Refreshment/Praktikum/task6.js
@@ -0,0 +1,4 @@
+let arr = [1.5, 2.56, 5.1, 12.33];
+
+const NEWARR = arr.map((x) => Math.round(x));
+console.log(NEWARR);
